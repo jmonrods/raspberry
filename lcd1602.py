3ab27113382d17b155bd44a0e53a077f3b76582b
Juan Montero <jjmontero9@gmail.com>
2023-12-30 13:37:55 -0600

    added libs folder

--- lcd1602.py
+++ lcd1602.py
@@ -1,6 +1,6 @@
 from machine import Pin, I2C, ADC
 from time import sleep, sleep_ms
-from machine_i2c_lcd import I2cLcd
+from libs.machine_i2c_lcd import I2cLcd
 
 i2c = I2C(0, scl=Pin(9), sda=Pin(8), freq=400000)
 addr = i2c.scan()[0]
